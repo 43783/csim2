d2567c380d08b5152f9df7311da15bb4bc1f023b
Eric Harth <eric.harth@hesge.ch>
2015-05-28 23:46:59 +0200

    test

--- ch.hesge.csim2.engine.methodconceptmatcher/src/ch/hesge/csim2/engine/MethodConceptMatcher.java
+++ ch.hesge.csim2.engine.methodconceptmatcher/src/ch/hesge/csim2/engine/MethodConceptMatcher.java
@@ -284,7 +284,7 @@ public class MethodConceptMatcher implements IEngine {
 				
 				RealVector methodVector = methodVectorMap.get(sourceMethod.getKeyId());
 				
-				// Loop over all concepts AAA BBB DDD
+				// Loop over all concepts AAA BBB DDD EEE
 				for (int i = 0; i < tfidfMatrix.getRowDimension(); i++) {
 					
 					RealVector conceptVector = tfidfMatrix.getColumnVector(i);
